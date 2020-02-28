d594f7736fc8f89ac33bfa4e1b34b2e35211b872
andrep2208 <Andre@DESKTOP-596TC3J>
2020-02-28 23:12:38 +1300

    BIT504 Programming 1 : Contents : Module 2: Introduction to Java programming : Data Types : String data type

--- MyFirstProject/src/HelloWorld.java
+++ MyFirstProject/src/HelloWorld.java
@@ -1,11 +1,17 @@
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("this is coming from pc2);
+		//char c = 'ab';
+		Scanner scanner = new Scanner(System.in);
+		
+		int numberOne = scanner.nextInt();
+		System.out.println("This is your input number " + numberOne);
 	}
+
 	
 
 
