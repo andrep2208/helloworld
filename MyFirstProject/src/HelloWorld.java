db6d65255bb87d720e673be5a65ddeb554a1f138
andrep2208 <Andre@DESKTOP-596TC3J>
2020-02-28 22:21:12 +1300

    from pc

--- MyFirstProject/src/HelloWorld.java
+++ MyFirstProject/src/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("this is coming from laptop");
+		System.out.println("this is coming from pc1");
 	}
+	
+
 
 }
