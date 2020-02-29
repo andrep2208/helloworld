1f34a631ee2a39fd794836a316f5a8efb7d9358c
andrep2208 <40508266+andrep2208@users.noreply.github.com>
2020-02-29 17:18:46 +1300

    simple add 2 numbers

--- MyFirstProject/src/HelloWorld.java
+++ MyFirstProject/src/HelloWorld.java
@@ -5,11 +5,21 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//char c = 'ab';
 		Scanner scanner = new Scanner(System.in);
 		
-		int numberOne = scanner.nextInt();
-		System.out.println("This is your input number " + numberOne);
+		System.out.println("what is your first number");
+		
+		int a = scanner.nextInt();
+		
+		System.out.println("what is your second numner");
+		
+		int b = scanner.nextInt();
+		
+		int c = a + b;
+		
+		System.out.println("your total is " + c);
+		
+		
 	}
 
 	
